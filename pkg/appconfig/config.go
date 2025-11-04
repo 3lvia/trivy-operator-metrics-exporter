7ba8eea72138c40ab8d2e405b6d21dd29d3e67a3
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2025-11-04 17:07:40 +0100

    Support setting config as env variables

--- pkg/appconfig/config.go
+++ pkg/appconfig/config.go
@@ -2,6 +2,8 @@ package appconfig
 
 import (
 	"context"
+	"os"
+	"time"
 
 	"github.com/3lvia/trivy-operator-metrics-exporter/pkg/utils"
 	log "github.com/sirupsen/logrus"
@@ -17,6 +19,16 @@ type Config struct {
 	EnableVulnerabilityMetrics bool                  // required
 	EnableExposedSecretMetrics bool                  // required
 	EnableConfigAuditMetrics   bool                  // required
+	MetricsUpdateInterval      time.Duration         // required
+	ExporterRestartInterval    time.Duration         // required
+}
+
+func parseTimeWithDefault(value string, defaultValue time.Duration) (time.Duration, error) {
+	if value == "" {
+		return defaultValue, nil
+	}
+
+	return time.ParseDuration(value)
 }
 
 func CreateConfig(ctx context.Context) *Config {
@@ -38,14 +50,47 @@ func CreateConfig(ctx context.Context) *Config {
 		log_.Fatalf("Could not configure metrics: %+v", err)
 	}
 
+	enableVulnerabilityMetrics := os.Getenv("ENABLE_VULNERABILITY_METRICS") != "false"
+	if !enableVulnerabilityMetrics {
+		log_.Info("Vulnerability metrics are disabled via ENABLE_VULNERABILITY_METRICS=false")
+	}
+
+	enableExposedSecretMetrics := os.Getenv("ENABLE_EXPOSED_SECRET_METRICS") != "false"
+	if !enableExposedSecretMetrics {
+		log_.Info("Exposed secret metrics are disabled via ENABLE_EXPOSED_SECRET_METRICS=false")
+	}
+
+	enableConfigAuditMetrics := os.Getenv("ENABLE_CONFIG_AUDIT_METRICS") != "false"
+	if !enableConfigAuditMetrics {
+		log_.Info("Config audit metrics are disabled via ENABLE_CONFIG_AUDIT_METRICS=false")
+	}
+
+	metricsUpdateInterval, err := parseTimeWithDefault(
+		os.Getenv("METRICS_UPDATE_INTERVAL"),
+		15*time.Minute,
+	)
+	if err != nil {
+		log_.Fatalf("Could not parse METRICS_UPDATE_INTERVAL: %+v", err)
+	}
+
+	exporterRestartInterval, err := parseTimeWithDefault(
+		os.Getenv("EXPORTER_RESTART_INTERVAL"),
+		1*time.Hour,
+	)
+	if err != nil {
+		log_.Fatalf("Could not parse EXPORTER_RESTART_INTERVAL: %+v", err)
+	}
+
 	return &Config{
 		Environment:                environment,
 		Debug:                      debug,
 		Local:                      local,
 		KubernetesClient:           kubernetesClient,
 		ApplicationMetrics:         *applicationMetrics,
-		EnableVulnerabilityMetrics: true,
-		EnableExposedSecretMetrics: true,
-		EnableConfigAuditMetrics:   environment == "dev", // TODO: enable for all environments
+		EnableVulnerabilityMetrics: enableVulnerabilityMetrics,
+		EnableExposedSecretMetrics: enableExposedSecretMetrics,
+		EnableConfigAuditMetrics:   enableConfigAuditMetrics,
+		MetricsUpdateInterval:      metricsUpdateInterval,
+		ExporterRestartInterval:    exporterRestartInterval,
 	}
 }
